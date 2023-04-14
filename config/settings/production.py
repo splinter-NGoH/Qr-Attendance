c122b3ba4f193f27d84f51ff330f246c674d517b
Splinter-NGoH <ahmedmansy265@email.com>
2023-04-14 16:20:04 +0200

    aws

--- config/settings/production.py
+++ config/settings/production.py
@@ -54,7 +54,9 @@ SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
 
 # STORAGES
 # ------------------------------------------------------------------------------
-# https://django-storages.readthedocs.io/en/latest/#installation
+# https://django-storages.readthedocs.io/en/latest/#installationAWS_DEFAULT_ACL = None
+AWS_DEFAULT_ACL = None
+
 INSTALLED_APPS += ["storages"]  # noqa F405
 # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
 AWS_ACCESS_KEY_ID = os.getenv("DJANGO_AWS_ACCESS_KEY_ID")
@@ -92,8 +94,10 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # MEDIA
 # ------------------------------------------------------------------------------
 MEDIA_URL = f"https://{AWS_S3_ENDPOINT_URL}/media/"
-DEFAULT_FILE_STORAGE = "qr_code.utils.storages.MediaRootS3Boto3Storage"
+AWS_S3_FILE_OVERWRITE = False
 
+# DEFAULT_FILE_STORAGE = "qr_code.utils.storages.MediaRootS3Boto3Storage"
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 # EMAIL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
